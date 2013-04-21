da3bc87e9c06f066d4f6bb63f84b8c29956abd82
Taylor Hornby <havoc@defuse.ca>
2013-04-21 12:45:04 -0600

    - Add the upload.sh script I use to upload files for transfer.

---
+++ upload.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# A script for easily uploading files to a web folder.
+# Usage: upload <file to upload> [name in url]
+# The file will be uploaded, given [name in url] on the web server, and the
+# URL to the file will be printed and put on the clipboard.
+
+readonly SSH_USER=defuse
+readonly SSH_HOST=defuse.ca
+readonly SSH_PATH="~/www/tmp_w"
+readonly HTTP_URL="https://defuse.ca/tmp_w"
+
+if [ $# -eq 2 ]; then
+    scp "$1" "$SSH_USER@$SSH_HOST:$SSH_PATH/$2"
+    url="$HTTP_URL/$2"
+    echo "$url" | xclip -sel clipboard
+    echo "$url"
+elif [ $# -eq 1 ]; then 
+    scp "$1" "$SSH_USER@$SSH_HOST:$SSH_PATH/$1"
+    url="$HTTP_URL/$1"
+    echo "$url" | xclip -sel clipboard
+    echo "$url"
+else
+    echo "Usage: <file to upload> [name in url]"
+fi
